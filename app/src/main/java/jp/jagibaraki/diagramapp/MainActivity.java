00f858289ad659d0e802b2c1bbb0338b8ed0fe3f
Shimokawa Takahiro <takahiro.1828@gmail.com>
2014-10-29 23:56:49 +0900

    MaterialテーマをAPI21未満に適用する

--- app/src/main/java/jp/jagibaraki/diagramapp/MainActivity.java
+++ app/src/main/java/jp/jagibaraki/diagramapp/MainActivity.java
@@ -1,12 +1,12 @@
 package jp.jagibaraki.diagramapp;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
