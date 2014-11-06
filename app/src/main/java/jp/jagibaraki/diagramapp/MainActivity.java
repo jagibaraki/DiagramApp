8cc5f5674dbecae5eac51e3b83c278db1ab3dab5
Shimokawa Takahiro <takahiro.1828@gmail.com>
2014-11-07 01:47:15 +0900

    DrawerLayoutを追加する ActionBarでの実装

--- app/src/main/java/jp/jagibaraki/diagramapp/MainActivity.java
+++ app/src/main/java/jp/jagibaraki/diagramapp/MainActivity.java
@@ -1,19 +1,42 @@
 package jp.jagibaraki.diagramapp;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private DrawerLayout vDrawerLayout;
+    private ActionBarDrawerToggle vDrawerToggle;
+    private ListView vListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        initDrawer();
+
+        vListView = (ListView) findViewById(R.id.listview);
+        vListView.setAdapter(new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, new String[]{"menu 1", "menu 2", "menu 3"}));
     }
 
+    private void initDrawer() {
+        vDrawerToggle = new ActionBarDrawerToggle(this, vDrawerLayout, R.string.app_name, R.string.app_name);
+        vDrawerToggle.setDrawerIndicatorEnabled(true);
+        vDrawerLayout.setDrawerListener(vDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,6 +57,18 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        return vDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        vDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        vDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
