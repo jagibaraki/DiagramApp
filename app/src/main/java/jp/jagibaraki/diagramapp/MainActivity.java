cd2b3700f20582b2e7fc71e87843de2542f4076a
Shimokawa Takahiro <takahiro.1828@gmail.com>
2014-11-13 00:04:56 +0900

    DrawerLayoutを追加する

--- app/src/main/java/jp/jagibaraki/diagramapp/MainActivity.java
+++ app/src/main/java/jp/jagibaraki/diagramapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -21,6 +22,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         vDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         initDrawer();
